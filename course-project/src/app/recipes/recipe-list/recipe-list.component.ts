9bc4506049793ea4609e8cf1bcd468540feaf6d2
Benjamin Whitelaw <benjamin.whitelaw@ge.com>
2019-09-10 16:07:00 -0400

    first commit

---
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
+
+@Component({
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is just a test', 'https://images-gmi-pmc.edge-generalmills.com/b6a2a4e7-73f5-4aec-9bb6-f2b5054d65e6.jpg')
+    ];
+    
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
